714d2bbf426ec22e74e695ee78fb64e409d8341e
AllenReich <71098537+AllenReich@users.noreply.github.com>
2022-03-18 15:24:33 -0400

    Update TaskB.java

--- src/task/TaskB.java
+++ src/task/TaskB.java
@@ -1,10 +1,13 @@
 package task;
 
-public class TaskB implements Task
-{
-    @Override
-    public void execute()
-    {
+public class TaskB extends Task {
 
+    public TaskB(char client, int taskNum) {
+        super(client, taskNum);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskB {" + "Client = " + client + ", Task# = " + taskNum + '}';
     }
 }
