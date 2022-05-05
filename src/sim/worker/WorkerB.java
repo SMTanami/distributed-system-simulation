dbc835fe8a7ab7e0a575acc0da7e217536742b50
AllenReich <71098537+AllenReich@users.noreply.github.com>
2022-05-05 11:18:02 -0400

    Update WorkerB.java

--- src/sim/worker/WorkerB.java
+++ src/sim/worker/WorkerB.java
@@ -2,47 +2,52 @@ package sim.worker;
 
 import sim.task.Task;
 import sim.task.TaskB;
+import sim.component.Component;
+import sim.component.ComponentID;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Random;
 
-public class WorkerB {
+public class WorkerB implements Component {
 
-    public static void main(String[] args) {
+    private final Socket myWorkerSocket;
+    private final ComponentID COMPONENT_ID;
 
-        if (args.length != 2) {
-            System.err.println("Usage: java Client <hostName> <portNumber>");
-            System.exit(1);
+    public WorkerB(Socket workerSocket) {
+        myWorkerSocket = workerSocket;
+        Random RANDOM = new Random();
+        COMPONENT_ID = new ComponentID(this, RANDOM.nextInt());
+    }
+
+    @Override
+    public void notifyConductor() {
+        try (ObjectOutputStream objOut = new ObjectOutputStream(myWorkerSocket.getOutputStream())) {
+            objOut.writeObject(COMPONENT_ID);
         }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Could not send component ID to conductor");
+        }
+    }
 
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
-        
-        Random r = new Random();
-        int workerID = r.nextInt();
-        Task task;
-
+    private void work(Task task) {
         try (Socket socket = new Socket(hostName, portNumber);
-             PrintWriter sendID = new PrintWriter(socket.getOutputStream(), true);
              ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
-            sendID.println("b " + workerID);
-            
             while ((task = (Task) in.readObject()) != null) {
                 System.out.println("Received: " + task);
-                
+
                 if (task.getClass() == TaskB.class) {
                     Thread.sleep(2000);
-                    System.out.println("This sim.task should take 2 seconds.");
+                    System.out.println("This task should take 2 seconds.");
                 }
 
                 else {
                     Thread.sleep(10000);
-                    System.out.println("This sim.task should take 10 seconds.");
+                    System.out.println("This task should take 10 seconds.");
                 }
 
                 System.out.println("Completed: " + task);
@@ -64,5 +69,19 @@ public class WorkerB {
             System.err.println("Couldn't find the object's class.");
             System.exit(1);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            System.err.println("Usage: java Client <hostName> <portNumber>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        WorkerB b = new WorkerB(new Socket(hostName, portNumber));
+        b.notifyConductor();
     }
 }
