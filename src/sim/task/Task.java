38d3ea8d5e8ddd9b42b36a768f5e67cc0b293978
AllenReich <71098537+AllenReich@users.noreply.github.com>
2022-05-09 14:32:29 -0400

    Update Task.java

--- src/sim/task/Task.java
+++ src/sim/task/Task.java
@@ -2,6 +2,20 @@ package sim.task;
 
 import java.io.Serializable;
 
+import sim.client.Client;
+import sim.conductor.Conductor;
+import sim.worker.Worker;
+
+/**
+ * Each Task is identified by three attributes: 1) the {@link Client} who requested the Task,
+ * 2) the number assigned by the Client to this particular Task, and 3) the {@link TASK_TYPE}.
+ * This record is important for the Client to track which task it sent and was completed.
+ * It is also important for the {@link Conductor} to be able to assign a {@link Worker} to perform the Task
+ * and to notify the proper Client of the completed Task.
+ * @param clientID the Client requesting the Task
+ * @param taskID the unique number the Client assigned to the Task
+ * @param type the type of Task
+ */
 public record Task(int clientID, int taskID, TASK_TYPE type) implements Serializable {
 
     @Override
