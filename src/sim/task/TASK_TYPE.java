4bc9c23a744be24772c197f5fb0c28d52625f346
AllenReich <71098537+AllenReich@users.noreply.github.com>
2022-05-09 14:31:54 -0400

    Update TASK_TYPE.java

--- src/sim/task/TASK_TYPE.java
+++ src/sim/task/TASK_TYPE.java
@@ -1,5 +1,11 @@
 package sim.task;
 
+import sim.client.Client;
+
+/**
+ * There are two different types of {@link Task} that can be requested by a {@link Client}. In order to identify which
+ * type of Task is being requested, each Task contains a type.
+ */
 public enum TASK_TYPE {
     A,
     B
